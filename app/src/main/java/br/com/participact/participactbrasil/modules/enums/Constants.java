a5a82f94448858f788688c6cb80d4eea53045268
Claudio <calraiden@yahoo.com.br>
2022-02-09 12:22:08 -0300

    Add constants url to tests

--- app/src/main/java/br/com/participact/participactbrasil/modules/enums/Constants.java
+++ app/src/main/java/br/com/participact/participactbrasil/modules/enums/Constants.java
@@ -1,5 +1,8 @@
 package br.com.participact.participactbrasil.modules.enums;
 
 public interface Constants {
-    String HOST = "https://bagheera.participact.com.br/";
+    //String HOST = "https://bagheera.participact.com.br/"; // PROD
+    String HOST = "http://localhost:8080/"; // LOCALHOST, no OK, but need proxy in emulator
+//    String HOST = "http://192.168.0.149:8080/"; // IP , no EMULADOR OK, your ip machine. In windows, execute the DOS command ipconfig
+    //String HOST = "http://example.com:8080/"; // FAKE DOMAIN, no OK in PC
 }
